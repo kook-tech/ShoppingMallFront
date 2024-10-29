37a8583ab26d8423e1a57a804d26a07ef3317fd1
DESKTOP-TR1IQBS\sky <knkn9436@naver.com>
2024-10-29 23:25:36 +0900

    Feat. React Router를 이용한 레이아웃 구성 Navigate를 활용한 라우팅 처리 쿼리스트링 처리와 상황에 따른 라우팅 async/await과 axios를 통해 백엔드 api 서버와 통신 조회 페이지 개발

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import BasicLayout from "../layouts/BasicLayout";
+
+
+function AboutPage(props) {
+    return (
+        <BasicLayout>
+            <div className="bg-green-300">About Page</div>
+        </BasicLayout>
+    )
+}
+
+export default AboutPage;
