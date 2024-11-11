68bd75b72ed98ae5c4cc7605b16f89972a856a3e
DESKTOP-J4RPHFP\kook <knkn9436@naver.com>
2024-11-11 23:09:20 +0900

    Feat. 상품 목록/조회페이지 추가 및 라우팅 처리 상품 목록/조회 페이지 조회. 수정, 삭제기능 개발 및 테스트 완료

--- src/router/productsRouter.js
+++ src/router/productsRouter.js
@@ -5,7 +5,8 @@ import { Navigate } from "react-router-dom";
 const Loading = <div>Loading....</div>
 const ProductList = lazy(() => import("../pages/products/ListPage"))
 const ProductAdd = lazy(() => import("../pages/products/AddPage"))
-
+const ProductRead =lazy(() => import("../pages/products/ReadPage"))
+const ProductModify= lazy(() => import("../pages/products/ModifyPage"))
 const productsRouter =  () =>{
     return [
         {
@@ -19,6 +20,14 @@ const productsRouter =  () =>{
         {
             path:'add',
             element: <Suspense fallback={Loading}><ProductAdd/></Suspense>
+        },
+        {
+            path:'read/:pno',
+            element: <Suspense fallback={Loading}><ProductRead/></Suspense>
+        },
+        {
+            path:'modify/:pno',
+            element: <Suspense fallback={Loading}><ProductModify/></Suspense>
         },
     ]
 }
